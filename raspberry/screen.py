3eaf52f7ff7ea6675e2f8eac3e78e4cc683cdb34
Michael Baum <MichaelBaum333@gmail.com>
2021-05-27 16:37:53 +0300

    raspberry: add API for constants
    Add file for macros.
    
    Signed-off-by: Michael Baum <MichaelBaum333@gmail.com>

--- raspberry/screen.py
+++ raspberry/screen.py
@@ -1,31 +1,8 @@
 
-# from tkinter import *
+from consts import Consts
 import tkinter as tk
 from PIL import Image, ImageTk
 
-'''
-<color name="colorPrimary">#00BCD4</color>
-<color name="colorPrimaryLight">#62efff</color>
-<color name="colorPrimaryDark">#008ba3</color>
-<color name="colorSecondary">#e040fb</color>
-<color name="colorSecondaryLight">#ff79ff</color>
-<color name="colorSecondaryDark">#aa00c7</color>
-<color name="PrimaryTextColor">#212121</color>
-<color name="SecondaryText">#757575</color>
-<color name="DividerColor">#BDBDBD</color>
-'''
-
-COLOR_PRIMARY = '#00BCD4'
-COLOR_PRIMARY_LIGHT = '#62efff'
-COLOR_PRIMARY_DARK = '#008ba3'
-COLOR_PRIMARY_TEXT = '#212121'
-COLOR_SECONDARY = '#e040fb'
-COLOR_SECONDARY_LIGHT = '#ff79ff'
-COLOR_SECONDARY_DARK = '#aa00c7'
-COLOR_SECONDARY_TEXT = '#757575'
-COLOR_DIVIDER = '#BDBDBD'
-
-SCREEN_SIZE = '480x320'
 
 def first_screen():
     root = tk.Tk()
@@ -33,8 +10,8 @@ def first_screen():
     a = tk.Label(root,
                  text ="Wellcome!",
                  font=("Arial Bold", 30),
-                 bg=COLOR_PRIMARY,
-                 fg=COLOR_PRIMARY_TEXT,
+                 bg=Consts.COLOR_PRIMARY,
+                 fg=Consts.COLOR_PRIMARY_TEXT,
                  padx = 2,
                  pady = 2)
     a.config(anchor='center')
@@ -42,14 +19,14 @@ def first_screen():
     b = tk.Label(root,
                  text ="Please enter a barcode.",
                  font=("Arial Bold", 20),
-                 bg=COLOR_PRIMARY,
-                 fg=COLOR_PRIMARY_TEXT,
+                 bg=Consts.COLOR_PRIMARY,
+                 fg=Consts.COLOR_PRIMARY_TEXT,
                  padx = 2,
                  pady = 2)
     b.config(anchor='center')
     b.pack(expand=True)
-    root.geometry(SCREEN_SIZE)
-    root.configure(background=COLOR_PRIMARY)
+    root.geometry(Consts.SCREEN_SIZE)
+    root.configure(background=Consts.COLOR_PRIMARY)
     # btn = tk.Button(root, text="barcode is given")
     # btn.grid(column=1, row=0)
     root.mainloop()
@@ -60,34 +37,34 @@ def second_screen():
     a = tk.Label(root,
                  text ="Thank you, please wait..",
                  font=("Arial Bold", 20),
-                 bg=COLOR_SECONDARY,
-                 fg=COLOR_SECONDARY_TEXT,
+                 bg=Consts.COLOR_SECONDARY,
+                 fg=Consts.COLOR_SECONDARY_TEXT,
                  padx = 10,
                  pady = 5)
     a.config(anchor='center')
     a.pack(expand=True)
-    root.geometry(SCREEN_SIZE)
-    root.configure(background=COLOR_SECONDARY)
+    root.geometry(Consts.SCREEN_SIZE)
+    root.configure(background=Consts.COLOR_SECONDARY)
     # btn = tk.Button(root, text="barcode is given")
     # btn.grid(column=1, row=0)
     root.mainloop()
 
 def show_picture():
     root = tk.Tk()
-    root.geometry(SCREEN_SIZE)
-    root.configure(background=COLOR_DIVIDER)
-    frame1 = tk.Frame(master=root, width=160, height=320, bg=COLOR_DIVIDER)
+    root.geometry(Consts.SCREEN_SIZE)
+    root.configure(background=Consts.COLOR_DIVIDER)
+    frame1 = tk.Frame(master=root, width=160, height=320, bg=Consts.COLOR_DIVIDER)
     a = tk.Label(frame1,
                  text = "The list is updated saccessfully!",
                  font=("Arial Bold", 10),
-                 bg=COLOR_DIVIDER,
-                 fg=COLOR_PRIMARY_TEXT,
+                 bg=Consts.COLOR_DIVIDER,
+                 fg=Consts.COLOR_PRIMARY_TEXT,
                  wraplength=150,
                  justify='left')
     a.place( anchor='ne')
     a.pack(fill='both')
     frame1.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
-    frame2 = tk.Frame(master=root, width=320, height=320, bg=COLOR_DIVIDER)
+    frame2 = tk.Frame(master=root, width=320, height=320, bg=Consts.COLOR_DIVIDER)
     image1 = Image.open('materna.jpeg')
     image1 = image1.resize((280,280))
     test = ImageTk.PhotoImage(image1)
