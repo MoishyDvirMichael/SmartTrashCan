fc0967ce1961310758fd52af8ee833b2f9c694b2
Michael Baum <MichaelBaum333@gmail.com>
2021-05-27 20:08:15 +0300

    raspberry: add API for error screen

--- raspberry/screen.py
+++ raspberry/screen.py
@@ -5,6 +5,7 @@ from PIL import Image, ImageTk
 from consts import Consts
 from wellcome_screen import Wellcome_screen
 from waitting_screen import Waitting_screen
+from error_screen import Error_screen
 
 
 def first_screen():
@@ -20,6 +21,14 @@ def second_screen():
     root.geometry(Consts.SCREEN_SIZE)
     root.configure(background=Consts.COLOR_BG_WAITTING)
     screen = Waitting_screen(master=root)
+    screen.show_screen()
+    root.mainloop()
+
+def third_screen():
+    root = tk.Tk()
+    root.geometry(Consts.SCREEN_SIZE)
+    root.configure(background=Consts.COLOR_BG_ERROR)
+    screen = Error_screen(master=root)
     screen.show_screen()
     root.mainloop()
 
@@ -52,6 +61,7 @@ def show_picture():
 def main():
     first_screen()
     second_screen()
+    third_screen()
     show_picture()
 
 
