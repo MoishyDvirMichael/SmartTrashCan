3183f3b6c486584b96a1f551cce18cf0bc4e4c52
Moishy Feder <moishy1010@gmail.com>
2021-06-08 17:09:03 +0300

    Deploying function on the server

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,24 +3,24 @@ import * as functions from 'firebase-functions';
 
 import { firestore } from 'firebase-admin';
 import * as admin from 'firebase-admin';
-import * as puppeteer from 'puppeteer';
+// import * as puppeteer from 'puppeteer';
 import { Product } from './Product';
 
 admin.initializeApp();
-const db = admin.firestore();
-let unknownRecyclingBinType: firestore.DocumentReference<firestore.DocumentData>;
-initializeDatabase();
+// const db = admin.firestore();
+// let unknownRecyclingBinType: firestore.DocumentReference<firestore.DocumentData>;
+// initializeDatabase();
 
-export const addBarcode = functions.https.onRequest(async (req, res) => {
-    const barcode = req.query.barcode;
-    const data = {
-        barcode: barcode,
-        is_identified: false,
-        date_added: admin.firestore.Timestamp.now(),
-    };
-    const writeResult = await db.collection('users/moishy/scanned_products').add(data);
-    res.json({ result: `Message with ID: ${writeResult.id} added.` });
-});
+// export const addBarcode = functions.https.onRequest(async (req, res) => {
+//     const barcode = req.query.barcode;
+//     const data = {
+//         barcode: barcode,
+//         is_identified: false,
+//         date_added: admin.firestore.Timestamp.now(),
+//     };
+//     const writeResult = await db.collection('users/moishy/scanned_products').add(data);
+//     res.json({ result: `Message with ID: ${writeResult.id} added.` });
+// });
 
 export const findDataForBarcode = functions.firestore.document('/users/{user}/scanned_products/{documentId}')
     .onCreate(async (snap, context) => {
@@ -29,13 +29,14 @@ export const findDataForBarcode = functions.firestore.document('/users/{user}/sc
         let is_identified;
         let product_id = null;
 
-        const product = await getDocument(db.collection('products').where('barcode', '==', barcode).limit(1));
+        const product = await getDocument(admin.firestore().collection('products').where('barcode', '==', barcode).limit(1));
         if (product !== null) {
             product_id = product;
             is_identified = true;
         }
         else {
-            const new_product = await scrapeProduct(barcode);
+            // const new_product = await scrapeProduct(barcode);
+            const new_product = null;
             if (new_product !== null) {
                 product_id = await addNewProduct(new_product);
                 is_identified = true;
@@ -56,40 +57,40 @@ async function getDocument(doc_ref: firestore.Query) {
 }
 
 async function addNewProduct(product: Product) {
-    const res = await db.collection('products').add(product);
+    const res = await admin.firestore().collection('products').add(product);
     return res;
 }
 
-async function scrapeProduct(barcode: number) {
-    const url = `https://www.rami-levy.co.il/he/shop?item=${barcode}`;
-    const browser = await puppeteer.launch();
-    let return_value = null;
-    try {
-        const page = await browser.newPage();
-        await page.goto(url);
+// async function scrapeProduct(barcode: number) {
+//     const url = `https://www.rami-levy.co.il/he/shop?item=${barcode}`;
+//     const browser = await puppeteer.launch();
+//     let return_value = null;
+//     try {
+//         const page = await browser.newPage();
+//         await page.goto(url);
+//
+//         const raw_image = await page.$eval('.product-img.mt-5', (e) => e.outerHTML);
+//         const images_url = raw_image.split('&quot;');
+//         if (images_url.length != 3) {
+//             throw `Can't find image in ${url}`;
+//         }
+//         const image_url = images_url[1];
+//         const name = await page.$eval('.h2.mt-4.mt-md-0', (e) => e.innerHTML);
+//         const images = [image_url];
+//         const recycling_bin_type = unknownRecyclingBinType;
+//         return_value = {
+//             barcode,
+//             name,
+//             images,
+//             recycling_bin_type,
+//         };
+//     } finally {
+//         browser.close();
+//         return return_value;
+//     }
+// }
 
-        const raw_image = await page.$eval('.product-img.mt-5', (e) => e.outerHTML);
-        const images_url = raw_image.split('&quot;');
-        if (images_url.length != 3) {
-            throw `Can't find image in ${url}`;
-        }
-        const image_url = images_url[1];
-        const name = await page.$eval('.h2.mt-4.mt-md-0', (e) => e.innerHTML);
-        const images = [image_url];
-        const recycling_bin_type = unknownRecyclingBinType;
-        return_value = {
-            barcode,
-            name,
-            images,
-            recycling_bin_type,
-        };
-    } finally {
-        browser.close();
-        return return_value;
-    }
-}
-
-async function initializeDatabase() {
-    let writeResult = await db.collection('recycling_bin_types').where('name', '==', 'unknown').get();
-    unknownRecyclingBinType = writeResult.docs[0].ref;
-}
+// async function initializeDatabase() {
+//     let writeResult = await db.collection('recycling_bin_types').where('name', '==', 'unknown').get();
+//     unknownRecyclingBinType = writeResult.docs[0].ref;
+// }
